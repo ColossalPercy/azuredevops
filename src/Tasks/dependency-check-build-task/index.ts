ec280c0a56303964117534e691eda373babd94b7
ColossalPercy <jackspercy@gmail.com>
2020-04-16 21:37:19 +0100

    get task inputs

--- src/Tasks/dependency-check-build-task/index.ts
+++ src/Tasks/dependency-check-build-task/index.ts
@@ -1,11 +1,23 @@
-import tl = require('azure-pipelines-task-lib/task');
+import task = require('azure-pipelines-task-lib/task');
 
 async function run() {
     try {
-        
+        const projectName: string = task.getInput('projectName', true);
+        const scanPath: string = task.getPathInput('scanPath', true);
+        const excludePath: string = task.getPathInput('excludePath');
+        const formats: string[] = task.getDelimitedInput('format', ',', true);
+        const failOnCVSS: string = task.getInput('failOnCVSS');
+        const suppressionPath: string = task.getPathInput('suppressionPath');
+        const enableExperimental: boolean = task.getBoolInput('enableExperimental', true);
+        const enableRetired: boolean = task.getBoolInput('enableRetired', true);
+        const enableVerbose: boolean = task.getBoolInput('enableVerbose', true);
+        const dataMirrorJson: string = task.getInput('dataMirrorJson');
+        const dataMirrorOdc: string = task.getInput('dataMirrorOdc');
+        const additionalArguments: string = task.getInput('additionalArguments');
+
     }
     catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
+        task.setResult(task.TaskResult.Failed, err.message);
     }
 }
 
