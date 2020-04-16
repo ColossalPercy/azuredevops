4749cab314c8f231a3914f8e797d020f4713815e
ColossalPercy <jackspercy@gmail.com>
2020-04-16 22:50:31 +0100

    set up arguments for cli

--- src/Tasks/dependency-check-build-task/index.ts
+++ src/Tasks/dependency-check-build-task/index.ts
@@ -2,6 +2,7 @@ import task = require('azure-pipelines-task-lib/task');
 
 async function run() {
     try {
+        // Get inputs from build task.
         const projectName: string = task.getInput('projectName', true);
         const scanPath: string = task.getPathInput('scanPath', true);
         const excludePath: string = task.getPathInput('excludePath');
@@ -14,6 +15,64 @@ async function run() {
         const dataMirrorJson: string = task.getInput('dataMirrorJson');
         const dataMirrorOdc: string = task.getInput('dataMirrorOdc');
         const additionalArguments: string = task.getInput('additionalArguments');
+
+        // Create reports directory
+        const testDirectory: string = task.getVariable('Common.TestResultsDirectory');
+        const reportsDirectory: string = testDirectory + '/dependency-check';
+
+        // Check if report directory does not exist
+        if (!task.exist(reportsDirectory)) {
+            console.log(`Creating dependency check test results directory at ${reportsDirectory}`);
+            task.mkdirP(reportsDirectory);
+        }
+
+        // Default args
+        let args: string[] = [];
+        args.push(`--project "${projectName}"`);
+        args.push(`--scan "${scanPath}"`);
+        args.push(`--out "${reportsDirectory}"`);
+
+        // Exclude switch
+        const localPath = task.getVariable('Build.Repository.LocalPath');
+        if (localPath !== excludePath) {
+            args.push(`--exclude "${excludePath}`);
+        }
+
+        // Format type
+        formats.forEach(f => {
+            args.push(`--format ${f}`);
+        })
+
+        // Fail on CVSS switch
+        if (typeof failOnCVSS !== 'undefined') {
+            let failOnCVSSValue = parseInt(failOnCVSS);
+            args.push(`--failOnCvss ${failOnCVSSValue}`);
+        }
+
+        // Suppression switch
+        if (localPath !== suppressionPath){
+            args.push(`--suppression "${suppressionPath}"`);
+        }
+
+        //Set enableExperimental option if requested
+        if (enableExperimental){
+            args.push('--enableExperimental');
+        }
+
+        //Set enableRetired option if requested
+        if (enableRetired){
+            args.push('--enableRetired');
+        }
+
+        //Set log switch if requested
+        if(enableVerbose) {
+            args.push(`--log "${reportsDirectory}/log"`);
+        }
+
+        // additionalArguments
+        if(typeof additionalArguments !== 'undefined') {
+            args.push(additionalArguments);
+        }
 
     }
     catch (err) {
